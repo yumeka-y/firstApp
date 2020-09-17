7b6dda2ff599e6494b37270dbe0ee55808ab7fcd
yumeka <yumeka.yabu@rakuten-sec.co.jp>
2020-09-17 19:00:20 +0900

    a

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <p>はじめてのwebサイトへようこそ</p>
+  </body>
+</html>
